5b12d406989817a20bb805caf9a83925aa1c7222
Ivan Montilla <ivan@iokode.net>
2025-01-20 14:05:48 +0100

    style: remove unused reference import

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import {z, defineCollection, reference} from "astro:content";
+import {z, defineCollection} from "astro:content";
 import {entriesGitHubLoader, simpleGitHubLoader} from "./EntriesGitHubLoader.ts";
 
 export const repository = 'iokode/blog-dev';
