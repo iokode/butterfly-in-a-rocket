c5e1ad0c3783e6feba114668514b4537d7def23f
Ivan Montilla <ivan@iokode.net>
2025-01-20 16:25:34 +0100

    feat: add RSS feed generation support
    Integrate RSS generation using @astrojs/rss for blog entries.

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,23 @@
+import rss from '@astrojs/rss';
+import {getCollection} from "astro:content";
+
+export async function GET() {
+    const entries = await getCollection('entries');
+    
+    return rss({
+        title: 'IOKode — The opinionated tech blog.',
+        description: 'A software development blog.',
+        site: "https://iokode.blog",
+        trailingSlash: false,
+        items: entries.map(entry => {
+            return {
+                title: entry.data.title,
+                link: `post/${entry.data.slug}`,
+                pubDate: entry.data.publishDate,
+                author: entry.data.authorName,
+            }
+        }),
+        // (opcional) inyecta xml personalizado
+        customData: `<language>en</language>`,
+    });
+}
