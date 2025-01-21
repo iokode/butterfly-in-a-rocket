b2986b46fed2d8631b8915e49c4c054b41968560
Ivan Montilla <ivan@iokode.net>
2025-01-21 19:32:59 +0100

    feat: add sitemap integration and robots.txt
    Added sitemap configuration using @astrojs/sitemap integration. Created a robots.txt file to include the sitemap URL and enable search engine indexing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import mdx from "@astrojs/mdx";
 
 import svelte from "@astrojs/svelte";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), svelte()],
+    site: "https://iokode.blog",
+    integrations: [mdx(), svelte(), sitemap()],
     image: {
         domains: ['github.com', 'avatars.githubusercontent.com', 'iokode.blog']
     }
